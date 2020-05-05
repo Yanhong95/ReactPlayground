5a2080124459d2b9a4127d6fd2d8eb1c3cd96074
Yanhong95 <yanhongmain@gmail.com>
2020-05-05 14:21:27 -0400

    update

--- React-http/src/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL : 'https://jsonplaceholder.typicode.com'
-});
-
-instance.defaults.header.commom['Authorization'] = 'AUTH TIKEN FROM INSTANCE';
-
-export default instance;
