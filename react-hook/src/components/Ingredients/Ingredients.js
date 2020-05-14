6ce8fb06169020a37c14473cc1c817d9ed980d9f
Yanhong95 <yanhongmain@gmail.com>
2020-05-13 20:30:07 -0400

    Finished useState() and useEffect()

--- react-hook/src/components/Ingredients/Ingredients.js
+++ react-hook/src/components/Ingredients/Ingredients.js
@@ -1,16 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  useEffect(() => {
+    fetch('https://my-project-react-hook.firebaseio.com/ingredients.json').then(
+      response => response.json()
+    ).then(responseData => {
+      const loadedIngredients = [];
+      for (const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount
+        });
+      }
+      setUserIngredients(loadedIngredients);
+    });
+  }, []);
+
+  const addIngredientHandler = ingredient => {
+    fetch('https://my-project-react-hook.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' }
+    }).then(response => {
+      // console.log(response.json());
+      return response.json();
+    }).then(responseData => {
+      setUserIngredients(prevIngredients => [
+        ...prevIngredients,
+        { id: responseData.name, ...ingredient }
+      ]);
+    });
+  }
+
+  const removeIngredientHandler = id => {
+    setUserIngredients(prevIngredients => [
+      ...prevIngredients.filter(ingredient => ingredient.id !== id)
+    ]);
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+      />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
   );
